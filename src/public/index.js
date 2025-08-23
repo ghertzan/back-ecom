02ec4ea309391fdd18985aeb616d66cb4d324e67
Gabriel Hertzan <Gabriel.Hertzan@gmail.com>
2025-08-23 10:40:34 -0300

    ..

--- src/public/index.js
+++ src/public/index.js
@@ -1,15 +1,15 @@
 const socket = io();
 
-const productsDiv = document.getElementById("productsDiv");
+const productsList = document.getElementById("productsList");
 
 socket.on("products", (products) => {
 	const infoProducts = products.map((product) => {
-		return `<div id=${product.id} class='productDiv'>
+		return `<li id=${product.id} class='list-group-item'>
                     <p><strong>Nombre: </strong> ${product.title}</p>
                     <p><strong>Precio: </strong> ${product.price}</p>
                     <p><strong>Stock: </strong> ${product.stock}</p>
-                </div>`;
+                </li>`;
 	});
 
-	productsDiv.innerHTML = infoProducts;
+	productsList.innerHTML = infoProducts;
 });
--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import handlebars from "express-handlebars";
 import path from "path";
 import { errorHandler } from "./middleware/error-handler.js";
 import viewsRouter from "./routes/views-router.js";
+import productRouter from "./routes/product-router.js";
+import cartRouter from "./routes/cart-router.js";
 import { productManager } from "./managers/product-manager.js";
 
 const app = express();
@@ -17,6 +19,8 @@ app.set("view engine", "handlebars");
 app.set("views", path.join(process.cwd(), "src", "views"));
 
 app.use("/", viewsRouter);
+app.use("/api/products", productRouter);
+app.use("/api/carts", cartRouter);
 
 app.use(errorHandler);
 
