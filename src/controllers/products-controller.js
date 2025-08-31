4b6ff76c2fcc352b4ea7233aeaec2a00515c57d4
Gabriel Hertzan <gabriel.hertzan@gmail.com>
2025-08-30 22:10:04 -0300

    product controller terminado

--- src/controllers/products-controller.js
+++ src/controllers/products-controller.js
@@ -4,4 +4,64 @@ class ProductController {
 	constructor(manager) {
 		this.manager = manager;
 	}
+
+	getAll = async (req, res, next) => {
+		try {
+			const products = await this.manager.getAll();
+			res.json(products);
+		} catch (error) {
+			next(error);
+		}
+	};
+
+	getById = async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const product = await this.manager.getById(id);
+			if (!product) {
+				return res.staus(404).json({ message: "Product not found" });
+			}
+			res.json(product);
+		} catch (error) {
+			next(error);
+		}
+	};
+
+	create = async (req, res, next) => {
+		try {
+			const productData = res.body;
+			const newProduct = await this.manager.create(productData);
+			res.status(201).json(newProduct);
+		} catch (error) {
+			next(error);
+		}
+	};
+
+	update = async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const productData = req.body;
+			const updatedProduct = await this.manager.update(id, productData);
+			if (!updatedProduct) {
+				return res.status(404).json({ message: "Product not found" });
+			}
+			res.json(updatedProduct);
+		} catch (error) {
+			next(error);
+		}
+	};
+
+	delete = async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const deletedProduct = await this.manager.delete(id);
+			if (!deletedProduct) {
+				return res.status(404).json({ message: "Product not found" });
+			}
+		} catch (error) {
+			next(error);
+		}
+	};
 }
+
+export const productController = new ProductController(productManager);
